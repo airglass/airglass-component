a9d2309d96a2a7b6472119fdc218eb196eb3b175
lanserdi <lanserdi@163.com>
2019-09-21 10:05:32 +0800

    update Group
    Group中子元素的类型更改为任意类型，
    方便RendererManager管理渲染器中的子元素。

--- Group.ts
+++ Group.ts
@@ -1,19 +1,19 @@
 import Element from './Element';
 
 export default class Group {
-  children: Array<Element>;
+  children: Array<any>;
 
   constructor(){
     this.children = [];
   }
   // 可以一次性添加多个
   add(){
-    let children: Array<Element> = [].slice.call(arguments, 0);
+    let children: Array<any> = [].slice.call(arguments, 0);
     children.forEach((child: Element) => {
       this.children.push(child);
     })
   }
-  remove(child: Element){
+  remove(child: any){
     for(let i in this.children){
       if(child === this.children[i]){
         this.children.splice(+i, 1);
