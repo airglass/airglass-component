b1761007f65008e5d30a1d799dc5e19e037888ca
lanserdi <lanserdi@163.com>
2019-09-21 11:37:18 +0800

    update Item

--- Item.ts
+++ Item.ts
@@ -1,17 +1,31 @@
-import Ellipse from './Ellipse';
+import Shape from './Shape';
 
-export default class Item extends Ellipse {
+export default class Item extends Shape {
   name: string;
   dir: string;
   nameFontSize: number;
   margin: number;
+  style: string;
+  size: number;
 
   constructor(opts) {
     super(opts);
     this.name = opts.name || '';
     this.dir = opts.dir || 'LTR';
     this.nameFontSize = opts.nameFontSize || 12;
-    this.margin = opts.margin || this.width;
+    this.size = opts.size || 10;
+    this.margin = opts.margin || this.size * 1.5;
+    this.style = opts.style || 'ellipse';
+  }
+  updatePath() {
+    let path: Path2D = new Path2D();
+    if (this.style == 'ellipse') {
+      path.ellipse(this.x, this.y, this.size / 2, this.size / 2, 0, 0, Math.PI * 2, true);
+    }
+    if(this.style == 'rect'){
+      path.rect(this.x - this.size / 2, this.y - this.size / 2, this.size, this.size);
+    }
+    this.path = path;
   }
   draw(ctx: CanvasRenderingContext2D) {
     if (!ctx) throw new Error('need ctx');
