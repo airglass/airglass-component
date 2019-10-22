897f2e34ee4bd3158d9c196dbe059236a9486812
airglass <lanserdi@163.com>
2019-10-22 14:19:42 +0800

    local

--- main.ts
+++ main.ts
@@ -1,9 +1,21 @@
 import Airglass from './Airglass';
 import Renderable from './Renderable';
 import Effect from './Effect';
-import Interactable from './Interactable';
 import AlterProgress from './AlterProgress';
 import Progress from './Progress';
+
+function extend(extendedGlass, prototype) {
+  let Glass = function (params) {
+    extendedGlass.call(this, params);
+    prototype._constructor && prototype._constructor.call(this, params);
+  };
+  function foo(){}
+  foo.prototype = extendedGlass.prototype;
+  Glass.prototype = new foo();
+  Glass.prototype.constructor = Glass;
+  for (var i in prototype) Glass.prototype[i] = prototype[i];
+  return Glass;
+}
 
 export {
   Airglass,
@@ -11,5 +23,5 @@ export {
   AlterProgress,
   Effect,
   Renderable,
-  Interactable,
+  extend
 }
