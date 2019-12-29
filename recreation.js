16f925eba322ce2c604253441125f6a46684ff72
airglass <lanserdi@163.com>
2019-12-29 16:11:14 +0800

    添加recreation配置

---
+++ recreation.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "library": "airglass",
+  "libraryTarget": "umd",
+  "pages": [{
+    "input": "main.ts",
+    "output": {
+      "filename": "../www/app/public/lib/airglass"
+    }
+  }]
+}
